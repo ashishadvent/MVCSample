db34d4062123d72e2059f52f66fa09df9fae4dcd
Administrator <Administrator@Win7_64Bit-PC.AshuGemalto1.com>
2018-01-28 00:33:05 +0530

    Third Commit -> Multi RequestMapping annotations

--- Practice_Demo/src/com/practice/HelloController.java
+++ Practice_Demo/src/com/practice/HelloController.java
@@ -1,26 +1,34 @@
 package com.practice;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+
 
 
 
   @Controller
+  @RequestMapping("/greet")
     public class HelloController {
 	@RequestMapping("/welcome")
 	public ModelAndView getHello() {
 		
 		ModelAndView modelandview =new ModelAndView("HelloPage");
-		modelandview.addObject("welcomemessage","Hi User has  successfully deployed this web site on local host using annotation");
+		modelandview.addObject("welcomemessage","Hi User has now  successfully deployed this web site on local host using annotation");
 		
 		return modelandview;
 	}
+	@RequestMapping("/hi")
+	public ModelAndView getHi() {
+		
+		ModelAndView modelandview =new ModelAndView("HelloPage");
+		modelandview.addObject("welcomemessage","New Message for welcome SUer ");
+		
+		return modelandview;
 	
-	 
+	}
 
 }
