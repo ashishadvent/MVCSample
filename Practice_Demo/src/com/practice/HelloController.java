b3dd454ac55994afb1939b709d4c5756fd622899
Administrator <Administrator@Win7_64Bit-PC.AshuGemalto1.com>
2018-01-27 23:42:18 +0530

    Second Commit -> used Spring defined annotation

--- Practice_Demo/src/com/practice/HelloController.java
+++ Practice_Demo/src/com/practice/HelloController.java
@@ -3,18 +3,20 @@ package com.practice;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 
 
-
-    public class HelloController extends AbstractController{
-	@Override
-	public ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+  @Controller
+    public class HelloController {
+	@RequestMapping("/welcome")
+	public ModelAndView getHello() {
 		
 		ModelAndView modelandview =new ModelAndView("HelloPage");
-		modelandview.addObject("welcomemessage","Hi User i am successfully deployed this web site on local host");
+		modelandview.addObject("welcomemessage","Hi User has  successfully deployed this web site on local host using annotation");
 		
 		return modelandview;
 	}
